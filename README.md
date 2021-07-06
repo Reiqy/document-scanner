7f5b30bb84f6d91d2e3d7663da8e106a76f80138
Reiqy <kvetny.michal@gmail.com>
2021-07-06 16:07:02 +0200

    finished basic algorithm, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Document Scanner
+
+Simple CLI application for scanning documents from images. This application is build around algorithm proposed by Adrian Rosebrock [[1]](https://www.pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/) and uses OpenCV's adaptive thresholding [[2]](https://docs.opencv.org/4.5.2/d7/d4d/tutorial_py_thresholding.html) for scanner-like look of the output images.
+
+![Input Image](readme/1_mini.jpg)
+![Outut Image](readme/1_out_mini.jpg)
+
+_Source image on the left is converted into the output image on the right._
+
+## Usage
+
+To use Document Scanner you need to have Python installed with all the required packages as specified in `requirements.txt`. Run
+    
+    $ pip install -r requirements.txt
+
+Then you can run the script by calling
+
+    $ python scanner.py <arguments>
+
+There are several arguments that have to be specified for successful execution of the program. You need to specify the format of the program output. You can choose between
+
+1. `-v`, this option only displays the output images. Additionaly to this option you can optionally specify
+    - `-t <integer>`, target height of the displayed images on screen;
+    
+2. `-p <filename>`, this option creates a `.pdf` with specified `<filename>` (note that the name must have the `.pdf` extension). If you want you can optionally specify dimensions of the images in the generate file with
+    - `-d <integer> <integer>`, defaults for these options are numbers `210` and `297` (dimensions of A4 paper);
+    
+3. `-i <dirname>`, this option generates individual images in directory `<dirname>`.
+
+For all the options you can specify the `-s <integer1> <integer2>` which will apply post-processing effects to make the document look more like if it was scanned by conventional document scanner. Numbers `<integer1>` and `<integer2>` can either be zeros. This will cause the output images to be converted to grayscale. Or they can be positive and be used as arguments to the adaptive thresholding. First number stands for block size, second is constant `c`. Good results are obtained with numbers `11` and `10`.
+
+Then you have to specify at least one `<filename>` of input image.
+
+Full call of the script can look like this
+
+    $ python scanner.py -p out/test.pdf -s 11 10 data/1.jpg data/2.jpg
+
+Keep in mind that the command line arguments will likely change in future versions.
+
+## Development
+
+This project is still in development. Future goals are
+
+1. Better post-processing of the output images;
+
+2. Plain-text output.
+   
+## Reference
+
+[1] [Building Mobile Document Scanner by Adrian Rosebrock](https://www.pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/)
+
+[2] [Adaptive Thresholding](https://docs.opencv.org/4.5.2/d7/d4d/tutorial_py_thresholding.html)
---
+++ scanner.py
@@ -0,0 +1,196 @@
+import os.path
+import sys
+import cv2
+import argparse
+import fpdf
+import numpy as np
+import tempfile
+
+
+def parse_arguments(args=None):
+    parser = argparse.ArgumentParser(description="Random description", allow_abbrev=True)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-p", "--pdf", nargs=1)
+    group.add_argument("-v", "--view", action="store_true")
+    group.add_argument("-i", "--image", nargs=1)
+    parser.add_argument("-t", "--target", type=int, nargs=1)
+    parser.add_argument("-d", "--dimensions", type=int, nargs=2)
+    parser.add_argument("-s", "--scanner", type=int, nargs=2)
+    parser.add_argument("filenames", nargs="+")
+    return parser.parse_args(args)
+
+
+def convert_to_grayscale_img(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+def compute_resize_ratio(img, target, target_dimension="height"):
+    shape = img.shape
+    dimension = 0
+    if target_dimension == "height":
+        dimension = shape[0]
+    elif target_dimension == "width":
+        dimension = shape[1]
+    return dimension / target
+
+
+def resize_img(img, scale, interpolation=cv2.INTER_AREA):
+    return cv2.resize(img, (0, 0), fx=scale, fy=scale, interpolation=interpolation)
+
+
+def blur_img(img, size=(5, 5), sigma=1):
+    return cv2.GaussianBlur(img, size, sigma)
+
+
+def detect_edges(img, threshold1=50, threshold2=200):
+    return cv2.Canny(img, threshold1, threshold2)
+
+
+def detect_document(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    document_contour = None
+    document_approximation = None
+    for contour in contours:
+        perimeter = cv2.arcLength(contour, True)
+        approximation = cv2.approxPolyDP(contour, 0.01 * perimeter, True)
+
+        if len(approximation) == 4:
+            document_approximation = approximation
+            document_contour = contour
+            break
+
+    return document_approximation, document_contour
+
+
+def transform_to_birds_eye(img, document_approximation):
+    corner_points = document_approximation.reshape(4, 2).astype("float32")
+    corner_points = sorted(corner_points, key=lambda x: x[0])
+    left = sorted(corner_points[:2], key=lambda x: x[1])
+    right = sorted(corner_points[2:], key=lambda x: x[1], reverse=True)
+    corner_points = np.asarray(left + right)
+    tl, bl, br, tr = corner_points
+    width = int(max(np.linalg.norm(tl - tr), np.linalg.norm(bl - br)))
+    height = int(max(np.linalg.norm(tl - bl), np.linalg.norm(tr - br)))
+    target_points = np.array(
+        [
+            [0, 0],
+            [0, height],
+            [width, height],
+            [width, 0]
+        ], dtype="float32"
+    )
+    transformation_matrix = cv2.getPerspectiveTransform(corner_points, target_points)
+
+    return cv2.warpPerspective(img, transformation_matrix, (width, height))
+
+
+def correct_img(img):
+    grayscale = convert_to_grayscale_img(img)
+    ratio = compute_resize_ratio(grayscale, 500, "height")
+    grayscale = resize_img(grayscale, 1 / ratio)
+    grayscale = blur_img(grayscale)
+    edges = detect_edges(grayscale)
+    document_approximation, document_contour = detect_document(edges)
+    if document_approximation is None:
+        return None
+    # leaving the detailed contour so that maybe I can apply non-linear transformation to correct the resulting image
+    img = transform_to_birds_eye(img, document_approximation * ratio)
+
+    return img
+
+
+def scannify(img, block_size, c):
+    img = convert_to_grayscale_img(img)
+    if (block_size != 0) and (c != 0):
+        if block_size % 2 != 1:
+            error(f"Specified block size {block_size} isn't odd.")
+        img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, block_size, c)
+    return img
+
+
+def show_img(img, window_name, scale=1):
+    cv2.imshow(window_name, resize_img(img, scale))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def validate_filename(filename, extension):
+    return filename.split(".")[1] == extension
+
+
+def error(msg):
+    print("Error:", msg, "Aborting process.", file=sys.stderr)
+    sys.exit(1)
+
+
+def view(filenames, out_images, args):
+    if args.target:
+        height = args.target[0]
+    else:
+        height = 800
+
+    for i, (filename, img) in enumerate(zip(filenames, out_images)):
+        ratio = compute_resize_ratio(img, height)
+        show_img(img, f"{filename} ({i + 1})", 1 / ratio)
+
+
+def pdf(_filenames, out_images, args):
+    pdf_filename = args.pdf[0]
+    if not validate_filename(pdf_filename, "pdf"):
+        error(f"Name {pdf_filename} isn't valid name for .pdf file.")
+
+    if args.dimensions:
+        w, h = args.dimensions
+    else:
+        w = 210
+        h = 297
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        pdf_file = fpdf.FPDF()
+        pdf_file.compress = False
+        for i, img in enumerate(out_images):
+            img_filename = temp_dir + str(i) + ".png"
+            cv2.imwrite(img_filename, img)
+            pdf_file.add_page()
+            pdf_file.image(img_filename, x=0, y=0, w=w, h=h)
+
+        pdf_file.output(pdf_filename, "F")
+
+
+def image(filenames, out_images, args):
+    out_dir = args.image[0]
+    if not os.path.isdir(out_dir):
+        error(f"Directory {out_dir} doesn't exist.")
+
+    for filename, img in zip(filenames, out_images):
+        cv2.imwrite(out_dir + "/" + os.path.basename(os.path.normpath(filename)), img)
+
+
+def main(sample_arguments=None):
+    args = parse_arguments(sample_arguments)
+
+    filenames = args.filenames
+
+    out_images = []
+    for i, filename in enumerate(filenames):
+        img = cv2.imread(filename, cv2.IMREAD_COLOR)
+        if img is None:
+            error(f"Image {filename} ({i + 1}) doesn't exist.")
+        out_img = correct_img(img)
+        if out_img is None:
+            error(f"Couldn't detect document in image {filename} ({i + 1}).")
+        if args.scanner:
+            out_img = scannify(out_img, args.scanner[0], args.scanner[1])
+        out_images.append(out_img)
+
+    if args.view:
+        view(filenames, out_images, args)
+    elif args.pdf:
+        pdf(filenames, out_images, args)
+    elif args.image:
+        image(filenames, out_images, args)
+
+
+if __name__ == '__main__':
+    main()
